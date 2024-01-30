834097e88f2d7bac49d69759e6392af986e732e8
Eric Lin <ericlin071906@gmail.com>
2024-01-30 17:38:47 -0500

    change SwerveDriveWhileShooting based on ben's feedback

--- src/main/java/com/stuypulse/robot/commands/swerve/SwerveDriveWhileShooting.java
+++ src/main/java/com/stuypulse/robot/commands/swerve/SwerveDriveWhileShooting.java
@@ -1,8 +1,5 @@
 package com.stuypulse.robot.commands.swerve;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-
 import com.stuypulse.robot.constants.Settings;
 import com.stuypulse.robot.constants.Settings.Alignment;
 import com.stuypulse.robot.constants.Settings.Driver.Drive;
@@ -13,7 +10,6 @@ import com.stuypulse.stuylib.control.angle.feedback.AnglePIDController;
 import com.stuypulse.stuylib.input.Gamepad;
 import com.stuypulse.stuylib.math.Angle;
 import com.stuypulse.stuylib.math.Vector2D;
-import com.stuypulse.stuylib.network.SmartNumber;
 import com.stuypulse.stuylib.streams.vectors.VStream;
 import com.stuypulse.stuylib.streams.vectors.filters.VDeadZone;
 import com.stuypulse.stuylib.streams.vectors.filters.VLowPassFilter;
@@ -21,6 +17,7 @@ import com.stuypulse.stuylib.streams.vectors.filters.VRateLimit;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class SwerveDriveWhileShooting extends Command {
@@ -31,8 +28,8 @@ public class SwerveDriveWhileShooting extends Command {
     private final AngleController gyroFeedback;
     private final Pose2d target;
 
-    private LinkedList<Vector2D> velocities = new LinkedList<Vector2D>();
-    private SmartNumber magVelNote = new SmartNumber("Note Velocity", 8);//should be in settings
+   
+    private VStream robotSpeed;
 
     public SwerveDriveWhileShooting(Pose2d target, Gamepad driver) {
         this.swerve = SwerveDrive.getInstance();
@@ -46,28 +43,26 @@ public class SwerveDriveWhileShooting extends Command {
                     new VRateLimit(Drive.MAX_TELEOP_ACCEL),
                     new VLowPassFilter(Drive.RC)
                 );
-
+        
         this.gyroFeedback = new AnglePIDController(Alignment.Gyro.P.get(), Alignment.Gyro.I.get(), Alignment.Gyro.D.get());
         this.target = target;
+
+        this.robotSpeed = VStream.create(() -> chassisSpeedsVector())
+            .filtered(new VLowPassFilter(Settings.Swerve.VELOCITY_RC.get()));
 
         addRequirements(swerve);
     }
 
-
+    private Vector2D chassisSpeedsVector() {
+        return  new Vector2D(
+            swerve.getChassisSpeeds().vxMetersPerSecond, 
+            swerve.getChassisSpeeds().vyMetersPerSecond
+        );
+        
+    }
    
-    private Vector2D avgVel(){
-        //updating the linked list
-        velocities.add(speed.get());
-        if(velocities.size() > 10){
-            velocities.removeFirst();
-        }
-
-        Vector2D avgVel = new Vector2D(0, 0);
-        //calculate average velocity
-        for(Vector2D vel : velocities){
-            avgVel.add(vel);
-        }
-        return avgVel.div(velocities.size());
+    public Vector2D getVelocity(){
+        return robotSpeed.get();
     }
 
 
@@ -75,14 +70,10 @@ public class SwerveDriveWhileShooting extends Command {
     public void execute() {
         
         
-        Vector2D velNote = new Vector2D(
-            magVelNote.get() * Math.cos(swerve.getGyroAngle().getRadians()), 
-            magVelNote.get() * Math.sin(swerve.getGyroAngle().getRadians())
-        );
+        Vector2D velNote = new Vector2D(new Translation2d(Settings.Swerve.NOTE_VELOCITY, swerve.getGyroAngle())) // field relative 
+            .sub(getVelocity()) // get the true note velocity 
+            .sub(getVelocity()); // shoot where we inteded to
 
-        //subracting to get the true vector
-        Vector2D resultant = velNote.sub(avgVel());
-        Rotation2d angle = Rotation2d.fromRadians(Math.atan2(resultant.y, resultant.x));
        
 
         Pose2d robotPose = AbstractOdometry.getInstance().getPose();
@@ -90,7 +81,7 @@ public class SwerveDriveWhileShooting extends Command {
         Rotation2d target = new Rotation2d(
                                 robotPose.getX() - this.target.getX(), 
                                 robotPose.getY() - this.target.getY())
-                                .plus(angle);
+                                .plus(velNote.getAngle().getRotation2d()); // pointing at where we want it to
             
         double angularVel = -gyroFeedback.update(
                             Angle.fromRotation2d(target),
@@ -101,11 +92,3 @@ public class SwerveDriveWhileShooting extends Command {
        
     }
 }
-/*
- * get the average velocity for the past 10 ticks (0.2 seconds) as a vector
- * get the velocity of the speaker as a vector (you want x and y), which means 
- * you need the angle from the target to the current pose (multiply the velocity by sintheta and cos to get x and y)
- * subtract the robot vector from the note vector 
- * get the resulting vector's field relative angle
- * add that angle to the target angle 
- */
