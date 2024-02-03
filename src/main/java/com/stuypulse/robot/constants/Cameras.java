be73ae35e6a7ae8fa8fe2e5f150d217f269b7ef6
BenG49 <ben@goldfisher.com>
2024-02-02 23:00:45 -0500

    Fix port

--- src/main/java/com/stuypulse/robot/constants/Cameras.java
+++ src/main/java/com/stuypulse/robot/constants/Cameras.java
@@ -10,9 +10,25 @@ import com.stuypulse.robot.util.OV2311Camera;
 
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 
 public interface Cameras {
+
+    public interface Limelight {
+        String [] LIMELIGHTS = {
+            "limelight"
+        };
+
+        int[] PORTS = { 5800, 5801, 5802, 5803, 5804, 5805 };
+
+        Pose3d [] POSITIONS = new Pose3d[] {
+            new Pose3d(
+                new Translation3d(Units.inchesToMeters(0), 0, Units.inchesToMeters(0)),
+                new Rotation3d(0, Math.toRadians(0), Math.toRadians(0))
+            )
+        };
+    }
 
     public static final CameraConfig DEFAULT_CAMERA = new CameraConfig("samera1",
             new Pose3d(-Units.inchesToMeters(12),
--- src/main/java/com/stuypulse/robot/subsystems/vision/LLNoteVision.java
+++ src/main/java/com/stuypulse/robot/subsystems/vision/LLNoteVision.java
@@ -2,8 +2,8 @@ package com.stuypulse.robot.subsystems.vision;
 
 import static com.stuypulse.robot.constants.Cameras.Limelight.*;
 
-import com.stuypulse.robot.subsystems.odometry.Odometry;
-import com.stuypulse.robot.util.vision.Limelight;
+import com.stuypulse.robot.subsystems.odometry.AbstractOdometry;
+import com.stuypulse.robot.util.Limelight;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -31,7 +31,7 @@ public class LLNoteVision extends NoteVision {
             }
         }
 
-        note = Odometry.getInstance().getField().getObject("Note");
+        note = AbstractOdometry.getInstance().getField().getObject("Note");
 
         notePose = new Translation2d();
     }
@@ -78,7 +78,7 @@ public class LLNoteVision extends NoteVision {
         Translation2d sum = new Translation2d();
 
         for (Limelight limelight : limelights) {
-            Odometry odometry = Odometry.getInstance();
+            AbstractOdometry odometry = AbstractOdometry.getInstance();
 
             Translation2d limelightToNote = new Translation2d(limelight.getDistanceToNote(), Rotation2d.fromDegrees(limelight.getXAngle()));
 
